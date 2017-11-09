4186b65674c425f58e1e4f66a26ef697288f6a72
Aires Oliveira <aires_oliveira@hotmail.com>
2017-11-09 22:49:51 +0000

    BidService updated  and Mock added

--- src/org/academiadecodigo/enuminatti/auctionhunt/service/BidService.java
+++ src/org/academiadecodigo/enuminatti/auctionhunt/service/BidService.java
@@ -1,13 +1,14 @@
 package org.academiadecodigo.enuminatti.auctionhunt.service;
 
 import org.academiadecodigo.enuminatti.auctionhunt.auxiliary.Item;
+import org.academiadecodigo.enuminatti.auctionhunt.model.User;
 
 /**
  * Created by codecadet on 09/11/2017.
  */
 public interface BidService extends Service {
 
-    void bidItem(int bidMoney);
+    void bidOnItem(int id, int bidMoney, User user);
 
     void addItemtoAuction(Item item);
 }
---
+++ src/org/academiadecodigo/enuminatti/auctionhunt/service/MockBidService.java
@@ -0,0 +1,42 @@
+package org.academiadecodigo.enuminatti.auctionhunt.service;
+
+import org.academiadecodigo.enuminatti.auctionhunt.auxiliary.Item;
+import org.academiadecodigo.enuminatti.auctionhunt.model.User;
+
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * Created by codecadet on 09/11/2017.
+ */
+public class MockBidService implements BidService {
+
+    private HashMap<Integer, Item> items;
+    private int itemAddedtoAuction;
+
+    public MockBidService(HashMap<Integer, Item> items) {
+
+        this.items = items;
+    }
+
+    @Override
+    public void bidOnItem(int id, int bidMoney, User user) {
+
+        Item item = items.get(id);
+
+        if (user == null || user == item.getUser()
+                || bidMoney <= item.getActualBid()) {
+            return;
+        }
+
+        item.setActualBid(bidMoney);
+        item.setUser(user);
+    }
+
+    @Override
+    public void addItemtoAuction(Item item) {
+
+        items.put(itemAddedtoAuction,item);
+        itemAddedtoAuction++;
+    }
+}

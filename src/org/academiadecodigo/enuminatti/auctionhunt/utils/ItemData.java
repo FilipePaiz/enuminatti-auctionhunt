af599337b0a56e535bb825a9a5359c286a324f2e
Filipe Pais <vitor.filipe.pais@gmail.com>
2017-11-12 16:53:20 +0000

    ItemData with the basic save method

---
+++ src/org/academiadecodigo/enuminatti/auctionhunt/utils/ItemData.java
@@ -0,0 +1,28 @@
+package org.academiadecodigo.enuminatti.auctionhunt.utils;
+
+import java.io.*;
+
+/**
+ * Created by Someone who is not me on 12/11/17.
+ */
+public class ItemData {
+
+    public static void save(String file, String name, String url, String price) throws IOException {
+
+        BufferedReader read = new BufferedReader(new FileReader(file));
+
+        String line = "";
+        String result = "";
+
+        while ((line = read.readLine()) != null) {
+            result += line + "\n";
+        }
+
+        BufferedWriter save = new BufferedWriter(new FileWriter(file, true));
+
+        save.write(name + "\n" + url + "\n" + price);
+        save.newLine();
+        save.flush();
+        save.close();
+    }
+}

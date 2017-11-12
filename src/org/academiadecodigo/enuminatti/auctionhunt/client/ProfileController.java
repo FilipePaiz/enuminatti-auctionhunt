caf6b5c23e493a46b03e0b513e5308069c2d5c01
Filipe Pais <vitor.filipe.pais@gmail.com>
2017-11-12 21:08:51 +0000

    Changed the path of ItemData

--- src/org/academiadecodigo/enuminatti/auctionhunt/client/ProfileController.java
+++ src/org/academiadecodigo/enuminatti/auctionhunt/client/ProfileController.java
@@ -111,7 +111,7 @@ public class ProfileController implements Initializable {
     void onLogoutButtonPressed(ActionEvent event) {
 
         try {
-            ItemData.save("src/org/academiadecodigo/enuminatti/auctionhunt/data/ItemData", "Aires", "Subaru Imprenza", "resources/Subaru", "995");
+            ItemData.save("resources/ItemData", "Aires", "Subaru Imprenza", "resources/Subaru", "995");
         } catch (IOException e) {
             e.printStackTrace();
         }

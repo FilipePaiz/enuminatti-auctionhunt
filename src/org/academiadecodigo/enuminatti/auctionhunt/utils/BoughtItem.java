88615623b37935055577e43de7aa15c2b2810d20
Filipe Pais <vitor.filipe.pais@gmail.com>
2017-11-14 22:35:34 +0000

    Saving the new owner

---
+++ src/org/academiadecodigo/enuminatti/auctionhunt/utils/BoughtItem.java
@@ -0,0 +1,45 @@
+package org.academiadecodigo.enuminatti.auctionhunt.utils;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Someone who is not me on 14/11/17.
+ */
+public class BoughtItem {
+
+    public static void save(String file, String name, String itemName, String itemID, String price) throws IOException {
+
+        BufferedReader read = new BufferedReader(new FileReader(file));
+
+        BufferedWriter save = new BufferedWriter(new FileWriter(file, true));
+
+        int itemNumber = 0;
+
+        String line;
+
+        while ((line = read.readLine()) != null) {
+            if (line.contains("<------------------->")) {
+                itemNumber = itemNumber + 1;
+            }
+
+        }
+        save.write("ID: " + name + "\n" +
+                "Item: " + itemName + "\n" +
+                "Item ID: " + itemID + "\n" +
+                "Price: " + price + "€\n" +
+                "Date: " + getDateTime() + "\n<------------------->");
+
+        save.newLine();
+        save.flush();
+        save.close();
+    }
+
+    private static String getDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+}

4faf7510e5347a547b81f3a6aa366f2052ea9670
Ana Cergol <nanacergol@hotmail.com>
2017-11-17 02:26:14 +0000

    new ItemTableView and controller

---
+++ src/org/academiadecodigo/enuminatti/auctionhunt/client/ItemTableController.java
@@ -0,0 +1,30 @@
+package org.academiadecodigo.enuminatti.auctionhunt.client;
+
+/**
+ * Created by codecadet on 17/11/17.
+ */
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ItemTableController {
+
+    @FXML
+    private Label Item1;
+
+    @FXML
+    private Label item2;
+
+    @FXML
+    private Label item3;
+
+    @FXML
+    private Label itemPrice2;
+
+    @FXML
+    private Label itemPrice1;
+
+    @FXML
+    private Label itemPrice3;
+
+}
+
